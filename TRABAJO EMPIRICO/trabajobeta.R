7da1b65bb2f5fc8a4f9cf0fd889f506a77e52a92
marctotti5 <marctotti5@gmail.com>
2020-10-01 16:36:30 +0200

    sajkdjiafs

--- TRABAJO EMPIRICO/trabajobeta.R
+++ TRABAJO EMPIRICO/trabajobeta.R
@@ -32,7 +32,6 @@ muestreo <- function(alpha, beta){
 
 resultados <- muestreo(alpha = 2, beta = 2)
 
-
 datosgráfico <- inner_join(resultados$media_muestral , resultados$estadístico)
 
 gráfico_estadistico <- ggplot(data = datosgráfico, aes(x = Estadístico)) + geom_density(fill = "lightblue") + 
